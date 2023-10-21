152e4e81dbd7cb65ffbdea084b3f9d418aae899e
Adam Podoxin <31609249+AdamPodoxin@users.noreply.github.com>
2023-10-21 11:20:04 -0700

    Add firebase

---
+++ src/lib/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSydz6wKBaMXiGa4mh0LRtS49x0v4UYXw",
+  authDomain: "fall-hacks-2023.firebaseapp.com",
+  projectId: "fall-hacks-2023",
+  storageBucket: "fall-hacks-2023.appspot.com",
+  messagingSenderId: "719485100577",
+  appId: "1:719485100577:web:d6818ae308fcd2a7aa3960"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
