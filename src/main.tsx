2c9f043b5f9788919f9e4afb81216e1e6a063793
Edan <ewstasiuk03@gmail.com>
2023-10-21 17:38:16 -0700

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import Directory from './directory.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* { <App />} */}
-    <Directory />
+    { <App />}
   </React.StrictMode>,
 )
