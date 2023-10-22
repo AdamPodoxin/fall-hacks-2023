32e62ddaa5b97131a188a32dcc116fa5de8ab959
Edan <ewstasiuk03@gmail.com>
2023-10-21 19:18:21 -0700

    Minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import FormPage from './formPage.tsx'
+// import Directory from './directory.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    { <App />}
+    {/* <App /> */}
+    {/* <Directory /> */}
+    <FormPage />
   </React.StrictMode>,
 )
